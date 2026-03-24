7abf86acb6944a4f97e48b64ed9cfd330f1b4134
Angelique Ardena <130275454+elirvrrii@users.noreply.github.com>
2026-03-24 16:46:17 +0800

    various bug fixes

--- lib/screens/stray_checklist/checklist_card.dart
+++ lib/screens/stray_checklist/checklist_card.dart
@@ -24,8 +24,8 @@ class ChecklistCard extends StatelessWidget {
         padding: const EdgeInsets.all(12),
         decoration: BoxDecoration(
           color: isDone
-              ? const Color(0xFFE8A7A0)
-              : const Color(0xFFE8A7A0).withOpacity(0.7),
+              ? const Color(0xFFFFB5A7)
+              : const Color.fromARGB(255, 219, 216, 216).withOpacity(0.7),
           borderRadius: BorderRadius.circular(14),
           boxShadow: [
             BoxShadow(
